9e8816fc7a4f63a430b1da6d5cda67971ec8839f
an.pham <an.pham@fullertonhealth.com>
2025-10-19 12:33:03 +0700

    build version

---
+++ dist/index.umd.js
@@ -0,0 +1,1 @@
+(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e.ATUtilsFe={}))})(this,(function(e){"use strict";const t=(n,i)=>n+i;e.sum=t,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})}));
